b43dd1160b18e4c6945677cdaaf7b60c9d80cd8c
kedar05121998 <101327577+kedar05121998@users.noreply.github.com>
2022-08-27 01:29:40 +0530

    day-4 done

--- fab_bag/src/Allroutes/Allroute.jsx
+++ fab_bag/src/Allroutes/Allroute.jsx
@@ -2,8 +2,11 @@
 import { Route, Routes } from "react-router-dom";
 import Footer from "../components/Footer";
 import FrontPage from "../components/FrontPage";
+
+import Login from "../components/Login"
 import Navbar from "../components/Navbar";
 import Product from "../components/Product";
+import SignUp from "../components/Signup";
 
 
 
@@ -13,7 +16,8 @@ function AllRoutes(){
         <Navbar />
         <Routes>
             <Route path="/" element={<FrontPage/>} />
-            <Route path="/signup" element={<h1>Signup</h1>} />
+            <Route path="/signup" element={<SignUp/>} />
+            <Route path="/login" element={<Login />} />
             <Route path="/cart" element={<h1>cart</h1>} />
             <Route path="/Product" element={<Product/>} />
 
